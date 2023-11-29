60130efa491cada0be83a592f256e41d98ab4043
César C <cesar.castares@gmail.com>
2023-11-29 09:10:08 +0100

    wip: developing the dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -5,8 +5,41 @@ from main import main
 
 data: DataFrame = main()
 
+dimensions_columns = [
+    "Nombre de Comunidad",
+    "Código de Provincia",
+    "Código de Municipio",
+    "Población",
+    "Número de mesas",
+    "Total censo electoral",
+    "Total votantes",
+    "Votos válidos",
+    "Votos a candidaturas",
+    "Votos en blanco",
+    "Votos nulos",
+]
+
+global_metrics_columns = [
+    "Número de mesas",
+    "Total censo electoral",
+    "Total votantes",
+    "Votos válidos",
+    "Votos a candidaturas",
+    "Votos en blanco",
+    "Votos nulos",
+]
+
+candidatures_columns = [
+    column
+    for column in data.columns
+    if column not in dimensions_columns + global_metrics_columns
+]
+
+
 st.dataframe(data)
 
+selected_columns = st.sidebar.multiselect('Select Columns', candidatures_columns)
+
 st.bar_chart(
-    data, y=["PSOE", "PP"], x="Nombre de Comunidad", color=["#ff2d00", "#0400ff"]
+    data, y=selected_columns, x="Nombre de Comunidad", #color=["#ff2d00", "#0400ff"]
 )
